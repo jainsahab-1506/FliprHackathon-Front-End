ab816216d7e04cf3d409d271c17148b4844feaa1
Naman Jain <2019UCP1390@mnit.ac.in>
2021-06-27 02:59:50 +0530

    login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,42 +1,65 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Login(props) {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === "email") {
+      setemail(value);
+    } else {
+      setpassword(value);
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
   return (
     <div>
-      <div className="content">
-        <div className="container login-container">
-          <div className="region">
-            <div className="login-card">
-              <h1>Login</h1>
-              <div className="form-container">
-                <form id="login-form">
-                  <div className="form-group">
-                    <label>Email</label>
-                    <input type="text" className="text-input" />
-                  </div>
-                  <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="text-input" />
-                  </div>
-                  <div className="submit-sec">
-                    <div className="submit-cont">
-                      <a href className="save-btn">
-                        Login
-                      </a>
-                    </div>
-                    <div className="submit-google-cont">
-                      <div className="g-signin2" data-onsuccess="onSignIn" />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <a href className="sign-up-link">
-                      Sign Up
-                    </a>
-                  </div>
-                </form>
+      <div className="login-card">
+        <h1>Login</h1>
+        <div className="form-container">
+          <form id="login-form">
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="text"
+                name="email"
+                onChange={handleChange}
+                value={email}
+                className="text-input"
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                className="text-input"
+              />
+            </div>
+            <div className="submit-sec">
+              <div className="submit-cont">
+                <a onClick={handleSubmit} href className="save-btn">
+                  Login
+                </a>
+              </div>
+              <div className="submit-google-cont">
+                <div className="g-signin2" data-onsuccess="onSignIn" />
               </div>
             </div>
-          </div>
+            <div className="form-group">
+              <Link to="/register" className="sign-up-link">
+                Sign Up
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,6 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Signup() {
+  const [email, setemail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setpassword] = useState("");
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === "email") {
+      setemail(value);
+    } else if (name === "firstName") {
+      setFirstName(value);
+    } else if (name === "lastName") {
+      setLastName(value);
+    } else {
+      setpassword(value);
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
   return (
     <div>
       <div className="signup-card">
@@ -9,31 +32,55 @@ export default function Signup() {
           <form id="login-form">
             <div className="form-group">
               <label>Email</label>
-              <input type="text" className="text-input" />
+              <input
+                type="text"
+                name="email"
+                value={email}
+                className="text-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="form-group">
               <label>Password</label>
-              <input type="password" className="text-input" />
+              <input
+                type="password"
+                name="password"
+                value={password}
+                className="text-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="form-group">
               <label>First Name</label>
-              <input type="text" className="text-input" />
+              <input
+                type="text"
+                name="firstName"
+                value={firstName}
+                className="text-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="form-group">
               <label>Last Name</label>
-              <input type="text" className="text-input" />
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                className="text-input"
+                onChange={handleChange}
+              />
             </div>
             <div className="submit-sec">
               <div className="submit-cont">
-                <a href className="save-btn">
+                <a onClick={handleSubmit} className="save-btn">
                   SignUp
                 </a>
               </div>
             </div>
             <div className="form-group">
-              <a href className="sign-up-link">
-                Sign in
-              </a>
+              <Link to="/login" className="sign-up-link">
+                Sign In
+              </Link>
             </div>
           </form>
         </div>
