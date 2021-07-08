3a1967bd544be467cf7c9a6b99613ceb26a33625
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2021-07-08 15:13:41 +0530

    deploy

--- src/Components/ChainComps/ManageChains.js
+++ src/Components/ChainComps/ManageChains.js
@@ -1,23 +1,15 @@
-<<<<<<< HEAD:src/Components/ManageChains.js
-import {React, useEffect} from 'react'
+import { React, useEffect } from "react";
 import { useSelector } from "react-redux";
-import ChainsContainer from './ChainComps/ChainsContainer';
-import Container from './Container';
-=======
-import React from 'react'
-import ChainsContainer from './ChainsContainer';
-import Container from '../Container';
->>>>>>> dev:src/Components/ChainComps/ManageChains.js
+import ChainsContainer from "./ChainComps/ChainsContainer";
+import Container from "./Container";
 
 export default function ManageChains() {
-    const authToken = useSelector((state) => state.auth.token);
-    useEffect(() => {
-      if (!authToken) {
-        // dispatch(logOutSuccess({}));
-        window.location.href = "/login";
-      };
-    });
-    return (
-        <Container children={<ChainsContainer/>}/>
-    )
+  const authToken = useSelector((state) => state.auth.token);
+  useEffect(() => {
+    if (!authToken) {
+      // dispatch(logOutSuccess({}));
+      window.location.href = "/login";
+    }
+  });
+  return <Container children={<ChainsContainer />} />;
 }
--- src/Components/EmailComps/ManageEmails.js
+++ src/Components/EmailComps/ManageEmails.js
@@ -1,25 +1,19 @@
-<<<<<<< HEAD:src/Components/ManageEmails.js
-import {React, useEffect} from 'react'
+import { React, useEffect } from "react";
 import { useSelector } from "react-redux";
-import Container from './Container';
-=======
-import React from 'react';
-
-import Container from '../Container';
->>>>>>> dev:src/Components/EmailComps/ManageEmails.js
-import EmailGroupTable from './EmailGroupComps/EmailGroupTable.jsx';
+import Container from "./Container";
+import EmailGroupTable from "./EmailGroupComps/EmailGroupTable.jsx";
 
 export default function ManageEmails() {
-	const authToken = useSelector((state) => state.auth.token);
+  const authToken = useSelector((state) => state.auth.token);
   useEffect(() => {
     if (!authToken) {
       // dispatch(logOutSuccess({}));
       window.location.href = "/login";
     }
   }, []);
-	return (
-		<div>
-			<Container children={<EmailGroupTable />} />
-		</div>
-	);
+  return (
+    <div>
+      <Container children={<EmailGroupTable />} />
+    </div>
+  );
 }
